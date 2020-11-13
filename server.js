e877000ce524d76ac7a9e745b68cc031cffe3eff
Cowleyfitcode <65091695+Mcowley1@users.noreply.github.com>
2020-11-12 21:59:46 -0500

    user room

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const formatMessage = require('./utils/message');
+const { userJoin, getCurrentUser } = require('./utils/user');
 
 const app = express();
 const server = http.createServer(app);
@@ -11,25 +13,31 @@ const formatMessage = require('./utils/message');
 // set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+const botName = 'LangMentor Bot';
+
 // run when client connects
 io.on('connection', socket => {
-    console.log('New ws Connection');
-    
-    // for single client
-    socket.emit('message', 'Welcome to LangMentor');
+    socket.on('joinRoom', ({ username, room }) => {
+        const user = userJoin(socket.id, username, room);
+        socket.join('user.room');
+
+            // for single client welcomes client
+    socket.emit('message', formatMessage(botName , 'Welcome to LangMentor'));
 
     // broadcast when user connects 
-    socket.broadcast.emit('message', 'A user has joined the chat');
+    socket.broadcast.emit('message', formatMessage(botName, 'A user has joined the chat'));
 
-    // Runs when client disconnects 
-    socket.on('disconnect', () => {
-        io.emit('message', 'A user has left the chat');
-    }); 
+    });
 
     // Listen for chatMessage
     socket.on('chatMessage', (msg) => {
-        io.emit('message', msg);
-    })
+        io.emit('message', formatMessage('USER', msg));
+    });
+
+    // Runs when client disconnects 
+    socket.on('disconnect', () => {
+        io.emit('message', formatMessage(botName, 'A user has left the chat'));
+    }); 
 });
 
 const PORT = 3000 || process.env.PORT;

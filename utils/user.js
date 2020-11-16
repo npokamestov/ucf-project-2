4c6b2c3c628698c923fcaf06e0f97d2098cbf265
Cowleyfitcode <65091695+Mcowley1@users.noreply.github.com>
2020-11-15 21:05:32 -0500

    add hover effect

--- utils/user.js
+++ utils/user.js
@@ -1,20 +1,36 @@
 const users = [];
 
-// join user to chat
+// Join user to chat
 function userJoin(id, username, room) {
-    const user = { id, username, room};
+  const user = { id, username, room };
 
-    user.push(user);
+  users.push(user);
 
-    return user;
+  return user;
 }
 
-// get current user
+// Get current user
 function getCurrentUser(id) {
-    return users.find(user => user.id === id);
+  return users.find(user => user.id === id);
+}
+
+// User leaves chat
+function userLeave(id) {
+  const index = users.findIndex(user => user.id === id);
+
+  if (index !== -1) {
+    return users.splice(index, 1)[0];
+  }
+}
+
+// Get room users
+function getRoomUsers(room) {
+  return users.filter(user => user.room === room);
 }
 
 module.exports = {
-    userJoin,
-    getCurrentUser
-}
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers
+};
